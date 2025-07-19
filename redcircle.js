6ce4ccd808a66f65cdf50ac3287e65a7c3a158a1
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-07-19 07:53:58 +0000

    Jul 19, 2025, 7:53 AM

---
+++ redcircle.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(600, 400);
+    background(220);
+}
+
+function draw(){
+    fill(255, 0, 0); //rgb
+    ellipse(300,100,100,100); // x-axis , y-axis, size
+    fill(255, 255, 0); //rgb
+    ellipse(300,200,100,100)
+    fill(0, 255, 0); //rgb
+    ellipse(300,300,100,100)
+}
