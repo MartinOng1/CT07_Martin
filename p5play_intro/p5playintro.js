b300cd00895b55ae063c46e48a87e91d33bc72d7
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-07-19 08:50:12 +0000

    Jul 19, 2025, 8:50 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -40,7 +40,7 @@ function draw() {
   // write your codes here
   background(240);
   fill(0);
-  if (ball.x < 0 + ball.diameter / 2 || ball.x > width) {
+  if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
 
   }
 }
