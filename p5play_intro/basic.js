f64d9a2b2fe7b76a86ead28bee09f6e465b3fc13
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-07-26 08:29:09 +0000

    Jul 26, 2025, 8:29 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -16,6 +16,12 @@ function setup() {
   for (let i = 1; i <=10; i++) {
     console.log(i)
   }
+  for (let i = 10; i <=10; i++) {
+    console.log(i)
+  }
+  for (let i = 1; i <=10; i++) {
+    console.log(i)
+  }
   for (let i = 1; i <=10; i++) {
     console.log(i)
   }
