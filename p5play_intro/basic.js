21f45772d3c9c43c0ac387ac7dc1333fcbeec814
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-07-26 09:04:35 +0000

    Jul 26, 2025, 9:04 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -54,7 +54,7 @@ function setup() {
   list.push("orange");
   list.push("butter");
   list.shift();
-  list.splice(1, "kaya");
+  list.splice("kaya", 1);
   console.log(list);
 }
 function draw() {
