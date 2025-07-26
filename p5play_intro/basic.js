d12218bae61c06e37500847959ad1c239f0b0664
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-07-26 08:07:02 +0000

    Jul 26, 2025, 8:07 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -18,17 +18,17 @@ function setup() {
   // write your codes here
 }
 function draw() {
-  noStroke();
-  fill(255, 0, 0);
-  ellipse(100, 100, 80, 50);
-  fill(0, 255, 0);
-  circle(200, 200, 30);
-  fill(0, 0, 255);
-  rect(30, 30, 55, 50);
-  noFill();
-  stroke(255, 0, 0);
-  triangle(30, 75, 58, 20, 86, 75);
-  noFill();
-  stroke(0, 0, 255);
-  quad(50, 162, 86, 150, 50, 138, 14, 150);
+  // noStroke();
+  // fill(255, 0, 0);
+  // ellipse(100, 100, 80, 50);
+  // fill(0, 255, 0);
+  // circle(200, 200, 30);
+  // fill(0, 0, 255);
+  // rect(30, 30, 55, 50);
+  // noFill();
+  // stroke(255, 0, 0);
+  // triangle(30, 75, 58, 20, 86, 75);
+  // noFill();
+  // stroke(0, 0, 255);
+  // quad(50, 162, 86, 150, 50, 138, 14, 150);
 }
