1e8d94e7bf3737d87d489fa1cae41cdc242a2d28
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-07-26 08:52:10 +0000

    Jul 26, 2025, 8:52 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -34,8 +34,8 @@ function setup() {
   // --- Exercise: Age category classification ---
   // write your codes here
   let age = 14;
-  if (age <= 9 {
-
+  if (age <= 9) {
+    
   }
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
