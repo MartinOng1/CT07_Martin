8d8cd7c1835275bd890964e2bc1453fce9d138c7
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-07-26 08:45:35 +0000

    Jul 26, 2025, 8:45 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -27,7 +27,7 @@ function setup() {
   // }
   let sum = 0;
   for (let i = 0; i <= 20; i+=2) {
-    let j = j + i;
+    j = j + i;
   }
   console.log(j);
   // --- Exercise: Age category classification ---
