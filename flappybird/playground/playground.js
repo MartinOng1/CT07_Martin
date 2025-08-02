9acc765145a921c24dbff8e856169791ffccf8de
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-08-02 08:36:58 +0000

    Aug 2, 2025, 8:36 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -3,6 +3,6 @@ let flapMidImg, bg, base;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
-    bg = loadImage('assets/background-day.png')
-    base = loadImage('assets/base.png')
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
 }
