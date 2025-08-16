60ea195a8f1a6e006a4d0367505cc3f557dc565f
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-08-16 08:05:26 +0000

    Aug 16, 2025, 8:05 AM

--- flappybird/playground/pg05a_spriteanimation.js
+++ flappybird/playground/pg05a_spriteanimation.js
@@ -5,6 +5,7 @@ function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
+    
 }
 
 function setup() {
