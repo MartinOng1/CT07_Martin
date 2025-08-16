23c3970628a4c9be631ac2471fee0055bc3b996e
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-08-16 08:14:18 +0000

    Aug 16, 2025, 8:14 AM

--- flappybird/playground/pg05a_spriteanimation.js
+++ flappybird/playground/pg05a_spriteanimation.js
@@ -46,6 +46,7 @@ function draw() {
         bird.rotation = -30;
     } else if (bird.vel > 1) {
         bird.img = flapDownImg;
+        bird.rotation = 30;
     }
     fill('black');
     textSize(14);
