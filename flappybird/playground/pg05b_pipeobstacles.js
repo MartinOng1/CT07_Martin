c370781f5a54d55c99b13cf40bf9cb1546435e80
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-08-16 08:34:09 +0000

    Aug 16, 2025, 8:34 AM

--- flappybird/playground/pg05b_pipeobstacles.js
+++ flappybird/playground/pg05b_pipeobstacles.js
@@ -71,5 +71,5 @@ function spawnPipePair() {
     let gap = 50;
     let midY = height / 2;
 
-    topPipe = new Sprite(400, midY-gap / 2 - 200);
+    topPipe = new Sprite(400, midY-gap / 2 - 200, 52, 320, 'static');
 }
