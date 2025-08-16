e5008ca52c9866d9c385507b7922a0f04a938469
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-08-16 09:11:13 +0000

    Aug 16, 2025, 9:11 AM

--- flappybird/playground/pg05b_pipeobstacles_copy.js
+++ flappybird/playground/pg05b_pipeobstacles_copy.js
@@ -80,5 +80,10 @@ function spawnPipePair() {
     pipeGroup.add(bottomPipe);
     pipeGroup.layer = 0;
     pipeGroup.vel.x = -2;
-    
+
+    for (let p of pipeGroup) {
+    if (p.x < -50) {
+        p.remove();
+    }
+}
 }
