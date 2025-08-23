8680de1e2faa551ef50b7eb38bcd5762a42a9541
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-08-23 09:24:52 +0000

    Aug 23, 2025, 9:24 AM

--- flappybird/playground/pg06_something.js
+++ flappybird/playground/pg06_something.js
@@ -84,6 +84,7 @@ function draw() {
         gameoverLabel.img = gameoverImg;
         gameoverLabel.layer = 100;
         gameoverLabel.x = camera.x;
+        gameoverLabel
     }
     // fill('black');
     // textSize(14);
