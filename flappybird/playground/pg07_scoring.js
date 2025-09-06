5d8b3f3cc6bd71adc837c0c8cc1fff9358117006
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-09-06 09:01:24 +0000

    Sep 6, 2025, 9:01 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -149,5 +149,8 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
 
 function moveGroup(group, targetX, spacing) {
     let totalWidth = (group.length - 1) * spacing;
-    let startX = (targetX - totalWidth/2)
+    let startX = (targetX - totalWidth/2);
+    for (let i = 0; i < group.length; i++) {
+        group[i].x = startX + i * spacing;
+    }
 }
